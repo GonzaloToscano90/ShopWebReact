0a062cafcfcdf6f91850f2804f56a89a53fc0f5f
GonzaloToscano90 <zalo.jat@gmail.com>
2022-06-07 00:44:30 -0300

    Config.inicial Proyecto ShopWebGames en ReactJS

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Header = () => {
+    const H3=styled.h3`
+        color:crimson;
+    `
+  return (
+    <H3 >Header</H3>
+  )
+}
+
+export default Header
